a345398c28b64b1bac9bc8e7fb4e67fdb67fce26
Marcelo Dias <mrclo.1999@gmail.com>
2025-02-16 16:59:15 +0000

    minor fix

--- app-frontend/src/app/shared/services/grid/grid.service.spec.ts
+++ app-frontend/src/app/shared/services/grid/grid.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GridService } from './grid.service';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
@@ -8,7 +7,8 @@ describe('GridService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClientTesting()],
+      imports: [],
+      providers: [GridService, provideHttpClientTesting()]
     });
     service = TestBed.inject(GridService);
   });
