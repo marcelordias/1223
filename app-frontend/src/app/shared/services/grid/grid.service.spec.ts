7faaab0e150b1c320cac2f2dd9c06897fa9ea75b
Marcelo Dias <mrclo.1999@gmail.com>
2025-02-16 16:56:27 +0000

    test fixs

--- app-frontend/src/app/shared/services/grid/grid.service.spec.ts
+++ app-frontend/src/app/shared/services/grid/grid.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GridService } from './grid.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('GridService', () => {
   let service: GridService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClientTesting()],
+    });
     service = TestBed.inject(GridService);
   });
 
--- app-frontend/src/app/shared/services/socket/socket.service.spec.ts
+++ app-frontend/src/app/shared/services/socket/socket.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SocketService } from './socket.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('SocketService', () => {
   let service: SocketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClientTesting()],
+    });
     service = TestBed.inject(SocketService);
   });
 
