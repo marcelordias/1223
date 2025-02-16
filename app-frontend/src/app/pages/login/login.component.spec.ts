063aeb734915144f373223ccba039c06e1830440
Marcelo Dias <mrclo.1999@gmail.com>
2025-02-16 16:50:54 +0000

    test fixs

--- app-frontend/src/app/pages/login/login.component.spec.ts
+++ app-frontend/src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +8,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      providers: [provideHttpClientTesting()]
     })
     .compileComponents();
 
